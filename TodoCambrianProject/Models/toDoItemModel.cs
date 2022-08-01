7ed3a5ec7cd19267a72e6a66fbf253a71c52e244
Athul Babu <athulbabu@Athuls-Mac-mini.local>
2022-08-01 19:47:30 -0400

    added model class and db operations

---
+++ TodoCambrianProject/Models/toDoItemModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using Todo.Models;
+
+namespace TodoCambrianProject.Models
+{
+    public class ToDoItemModel
+    {
+        public List<TodoItem> TodoList { get; set; }
+        public TodoItem Todo { get; set; }
+    }
+}
+
